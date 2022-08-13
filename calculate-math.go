e30b6065d5bc1c44267efe6fb3f797ecde526180
fikri fadillah <fikrifadillah231@gmail.com>
2022-08-13 08:17:20 +0700

    Add function factorial in mdoule calculate

--- calculate-math.go
+++ calculate-math.go
@@ -3,3 +3,11 @@ package calculatemath
 func SquareRoot(number int) int {
 	return number * number
 }
+
+func Factorial(number int) int {
+	if number == 1 {
+		return 1
+	} else {
+		return number * Factorial(number-1)
+	}
+}
